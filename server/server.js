0677c176504543077538d5b54c14f88db6887665
Victor Freitas <victor.freitas@tqi.com.br>
2017-11-23 16:19:15 -0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -88,6 +88,19 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+//POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        console.log(token);
+        res.header('x-auth', token).send(user);
+    }).catch((e) => res.status(400).send(e));
+});
+
 app.listen(port, () => {
     console.log('Started on port ' + port);
 });
