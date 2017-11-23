212b419008ed1f5ee2ed3d69db2486e8ce0d82db
Victor Freitas <victor.freitas@tqi.com.br>
2017-11-23 14:47:23 -0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {mongoose} = require('./db/mongoose');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {mongoose} = require('./db/mongoose');
 var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -59,6 +60,30 @@ app.delete('/todos/:id', (req, res) => {
 
         res.send({todo});
     }).catch((err) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body }, {new: true}).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
